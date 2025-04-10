eb21d61f62ab7df2cb3dff7d3a377b68e791bcc4
Aaditya Kalra <aaditya030107@gmail.com>
2025-04-11 03:04:58 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,28 +1,25 @@
 #include<stdio.h>
-int main(){
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for(int i=0; i<n; i++){
-        if(i==0 || i==n){
-            if(arr[0]>arr[1]){
-                printf("%d",arr[0]);
-            }
-            if(arr[n]>arr[n-1]){
-                printf("%d",arr[n]);
-            }
-        }
-        else if((arr[i]>arr[i-1])&&(arr[i]>arr[i+1])){
-            printf("%d",arr[i]);
+    for(int i = 0; i < n; i++) {
+        if(i == 0 && arr[i] > arr[i + 1]) {
+            printf("%d\n", arr[i]);
             return 0;
         }
-        else{
-            printf("%d",-1);
+        else if(i == n - 1 && arr[i] > arr[i - 1]) {
+            printf("%d\n", arr[i]);
+            return 0;
+        }
+        else if(i > 0 && i < n - 1 && arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+            printf("%d\n", arr[i]);
             return 0;
         }
     }
+    printf("-1\n");
     return 0;
 }
